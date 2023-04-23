d381751392cf7338b041fe8fdb66ea6dff269130
Dillon <lindstdb@gmail.com>
2023-04-22 21:09:22 -0500

    adds schema.sql, homeRoutes.js

---
+++ public/js/editPost.js
@@ -0,0 +1,48 @@
+const updatePost = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+    const post_id = document.querySelector('#postid').getAttribute('data-id');
+  
+    if (title && content && post_id) {
+      const response = await fetch(`/api/posts/`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('#updateBtn')
+    .addEventListener('click', updatePost);
+  
+  
+  const deletePost = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('#deleteBtn')
+    .addEventListener('click', deletePost);
